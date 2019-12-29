301d398b1fbd994a9e9d6014811df74616db85aa
egusahiroaki <egusa.hiroaki@gmail.com>
2019-12-29 22:37:46 +0900

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
+    node: true
   },
   extends: "eslint:recommended",
   globals: {
+    require: true,
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
   },

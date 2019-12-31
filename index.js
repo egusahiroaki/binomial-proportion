4e356335eab979da75c4125947db96a5348e1452
egusahiroaki <egusa.hiroaki@gmail.com>
2019-12-31 19:29:39 +0900

    fix

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-const jStat = require("jstat");
+const jStat = require('jstat');
 
-const BinomialProportion = (count, nobs, alpha = 0.05, method = "normal") => {
+const BinomialProportion = (count, nobs, alpha = 0.05, method = 'normal') => {
   if (nobs === 0) {
     return {
-      lowerBound: 0, value: 0, upperBound: 0
-    }
+      lowerBound: 0,
+      value: 0,
+      upperBound: 0,
+    };
   }
 
   const z = -1 * jStat.normal.inv(alpha / 2, 0, 1);
   let p = count / nobs;
   let sd;
   switch (method) {
-    case "normal": {
+    case 'normal': {
       sd = z * Math.sqrt((p * (1 - p)) / nobs);
       return { lowerBound: p - sd, value: p, upperBound: p + sd };
     }
-    case "agresti_coull": {
+    case 'agresti_coull': {
       const pAc = (count + z ** 2 / 2) / (nobs + z ** 2);
       sd = z * Math.sqrt((pAc * (1 - pAc)) / (nobs + z ** 2));
       return { lowerBound: pAc - sd, value: p, upperBound: pAc + sd };
     }
-    case "wilson": {
+    case 'wilson': {
       const denominator = 1 + z ** 2 / nobs;
       const centreAdjustedProbability = p + (z * z) / (2 * nobs);
       const adjustedStandardDeviation = Math.sqrt(
-          (p * (1 - p) + z ** 2 / (4 * nobs)) / nobs
+        (p * (1 - p) + z ** 2 / (4 * nobs)) / nobs
       );
 
       const lowerBound =
-          (centreAdjustedProbability - z * adjustedStandardDeviation) /
-          denominator;
+        (centreAdjustedProbability - z * adjustedStandardDeviation) /
+        denominator;
       const upperBound =
-          (centreAdjustedProbability + z * adjustedStandardDeviation) /
-          denominator;
+        (centreAdjustedProbability + z * adjustedStandardDeviation) /
+        denominator;
       return { lowerBound, value: p, upperBound };
     }
   }
--- test/test-index.js
+++ test/test-index.js
@@ -1,30 +1,29 @@
-import test from "ava";
-import BinomialProportion from "../index";
+import test from 'ava';
+import BinomialProportion from '../index';
 
-test("total is 0", t => {
-    const result = BinomialProportion(50, 0, 0.05, "normal");
-    t.is(result.lowerBound, 0);
-    t.is(result.value, 0);
-    t.is(result.upperBound, 0);
-  });
-  
+test('total is 0', t => {
+  const result = BinomialProportion(50, 0, 0.05, 'normal');
+  t.is(result.lowerBound, 0);
+  t.is(result.value, 0);
+  t.is(result.upperBound, 0);
+});
 
 test("normal: Binominal(10, 500, 0.05, 'normal')", t => {
-  const result = BinomialProportion(50, 500, 0.05, "normal");
+  const result = BinomialProportion(50, 500, 0.05, 'normal');
   t.is(result.lowerBound, 0.07370432378270254);
   t.is(result.value, 0.1);
   t.is(result.upperBound, 0.12629567621729745);
 });
 
 test("normal: Binominal(10, 500, 0.05, 'agresti_coull')", t => {
-  const result = BinomialProportion(50, 500, 0.05, "agresti_coull");
+  const result = BinomialProportion(50, 500, 0.05, 'agresti_coull');
   t.is(result.lowerBound, 0.07650314487851813);
   t.is(result.value, 0.1);
   t.is(result.upperBound, 0.12959632749072367);
 });
 
 test("normal: Binominal(10, 500, 0.05, 'wilson')", t => {
-  const result = BinomialProportion(50, 500, 0.05, "wilson");
+  const result = BinomialProportion(50, 500, 0.05, 'wilson');
   t.is(result.lowerBound, 0.07667756273730171);
   t.is(result.value, 0.1);
   t.is(result.upperBound, 0.1294219096319401);

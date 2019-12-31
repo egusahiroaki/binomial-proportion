62766cf1a0098a24a549f72fd1a2e32da1798d9d
egusahiroaki <egusa.hiroaki@gmail.com>
2019-12-31 19:29:07 +0900

    add prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,15 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: "eslint:recommended",
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
   globals: {
     require: true,
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
   },
+//   plugins: [
+//     "prettier"
+//   ],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: "module"
